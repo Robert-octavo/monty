c3d6a96e054632d4d7a21740654144f3efcb7eec
rmbodegalibre <rmbodegalibre@gmail.com>
2022-05-18 09:55:01 -0500

    get_function.c passed by betty

--- get_function.c
+++ get_function.c
@@ -1,18 +1,18 @@
 #include "monty.h"
 /**
  * get_function - Get the function object
- * 
+ *
  */
-void get_function()
+void get_function(void)
 {
-    instruction_t function_list[] = {
-        {"push", _push},
-        {"pall", _pall},
-        {"pint", _pint},
-        {"pop", _pop},
-        {"swap", _swap},
-        {"nop", _nop},
-        {"add", _add},
-        {NULL, NULL}
-    };
+	instruction_t function_list[] = {
+		{"push", _push},
+		{"pall", _pall},
+		{"pint", _pint},
+		{"pop", _pop},
+		{"swap", _swap},
+		{"nop", _nop},
+		{"add", _add},
+		{NULL, NULL}
+	};
 }

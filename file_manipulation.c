bccb1a9b8bf1d88c61fcfcd68ae530d55db890da
Robert-octavo <robert.ortega.octavo@gmail.com>
2022-05-18 00:56:16 -0500

    correct betty style

--- file_manipulation.c
+++ file_manipulation.c
@@ -2,39 +2,38 @@
 
 /**
  * o_file - FUnction that open a file
- * @file: Pointer to the argv[1] -(Path /String)
+ * @file_path: Pointer to the argv[1] -(Path /String)
  */
 void o_file(char *file_path)
 {
-    int file_exist = 0;
-    FILE *file;
+	int file_exist = 0;
+	FILE *file;
 
-    if (file_path == NULL)
-    {
-        printf("Error: Can't open file %s", file_path);
-        exit(EXIT_FAILURE);
-    }
+	if (file_path == NULL)
+	{
+		printf("Error: Can't open file %s", file_path);
+		exit(EXIT_FAILURE);
+	}
+/*file exist*/
+	file_exist = access(file_path, F_OK);
+/*if the return of access it is -1 - cannot acces the file*/
+	if (file_exist == -1)
+	{
+		printf("Error: Can't open file %s", file_path);
+		exit(EXIT_FAILURE);
+	}
 
-    /*file exist*/
-    file_exist = access(file_path, F_OK);
-    /*if the return of access it is -1 - cannot acces the file*/
-    if (file_exist == -1)
-    {
-        printf("Error: Can't open file %s", file_path);
-        exit(EXIT_FAILURE);
-    }
+	file = fopen(file_path, "r");
+	if (file == NULL)
+	{
+		printf("Error: Can't open file %s", file_path);
+		exit(EXIT_FAILURE);
+	}
 
-    file = fopen(file_path, "r");
-    if (file == NULL)
-    {
-        printf("Error: Can't open file %s", file_path);
-        exit(EXIT_FAILURE);
-    }
-
-    /* Read the File*/
-    r_file(file);
-    /*close the file*/
-    fclose(file);
+/* Read the File*/
+	r_file(file);
+/*close the file*/
+	fclose(file);
 }
 
 
@@ -45,24 +44,26 @@ void o_file(char *file_path)
  */
 void r_file(FILE *file)
 {
-    /*count the lines*/
-    int i = 0;
-    int check_token;
+	/*count the lines*/
+	int i = 0;
+	int check_token;
 
-    char *line = NULL;
-    size_t n = 0;
-    check_token = 0;
+	char *line = NULL;
+	size_t n = 0;
 
-    if (file == NULL)
-    {
-        printf("Error: Can't Open file");
-    }
+	check_token = 0;
 
-    for (i = 1; getline(&line, &n, file) != EOF; i++)
-    {   /*Test to Know if we are readind the files*/
-        /*printf("%s", line);*/
-        check_token = tokenizar(line, i);
-        printf("\n from main: %d ", check_token);
-    } 
-    free(line);
+	if (file == NULL)
+	{
+		printf("Error: Can't Open file");
+	}
+
+	for (i = 1; getline(&line, &n, file) != EOF; i++)
+	{
+/*Test to Know if we are readind the files*/
+/*printf("%s", line);*/
+		check_token = tokenizar(line, i);
+		printf("\n from main: %d ", check_token);
+	}
+	free(line);
 }

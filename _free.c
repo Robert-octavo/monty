2554d46803379a3d0d72a70e342556682d8e1d69
Robert-octavo <robert.ortega.octavo@gmail.com>
2022-05-19 00:41:00 -0500

    betty corrections

--- _free.c
+++ _free.c
@@ -4,13 +4,15 @@
  */
 void _free(void)
 {
-    stack_t *temp;
-    if (head == NULL)
-        return;
-    while (head != NULL)
-    {
-        temp = head;
-        head = head->next;
-        free(temp);
-    }
+	stack_t *temp;
+
+	if (head == NULL)
+		return;
+
+	while (head != NULL)
+	{
+		temp = head;
+		head = head->next;
+		free(temp);
+	}
 }

f8e3c7c4db02119104a2567a55f180632a8660b2
Robert-octavo <robert.ortega.octavo@gmail.com>
2022-05-19 00:37:54 -0500

    add _free function

---
+++ _free.c
@@ -0,0 +1,16 @@
+#include "monty.h"
+/**
+ * _free - Free nodes
+ */
+void _free(void)
+{
+    stack_t *temp;
+    if (head == NULL)
+        return;
+    while (head != NULL)
+    {
+        temp = head;
+        head = head->next;
+        free(temp);
+    }
+}

327419e329ca1508f3166a863e2428bfb9de2d71
rmbodegalibre <rmbodegalibre@gmail.com>
2022-05-18 21:22:12 -0500

    _pall.c passeb by Betty

--- _pall.c
+++ _pall.c
@@ -1,21 +1,21 @@
 #include "monty.h"
 /**
  * _pall - function that prints all the stack
- * @new: Pointer to the top node of the stack
+ * @stack: Is the first attribute
  * @i: Line number
  */
 void _pall(stack_t  **stack, __attribute__((__unused__)) unsigned int i)
 {
 /*check if it enter to the function*/
 /*   printf("enter pall: %d", i);*/
-    stack_t *tmp;
+	stack_t *tmp;
 /*   (void) i;*/
-    if (stack == NULL)
-        exit(EXIT_FAILURE);
-    tmp = *stack;
-    while (tmp != NULL)
-    {
-        printf("%d\n", tmp->n);
-        tmp = tmp->next;
-    }
+	if (stack == NULL)
+		exit(EXIT_FAILURE);
+	tmp = *stack;
+	while (tmp != NULL)
+	{
+		printf("%d\n", tmp->n);
+		tmp = tmp->next;
+	}
 }

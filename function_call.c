5f20db4e68bbb36f5bf6b33022c9654b23d5e5bd
rmbodegalibre <rmbodegalibre@gmail.com>
2022-05-18 22:02:11 -0500

    function_call.c passed by betty

--- function_call.c
+++ function_call.c
@@ -1,38 +1,43 @@
 #include "monty.h"
+/**
+ * function_call - Function to call functions
+ * @function: Nema of the function caller
+ * @code: Code taken fronm file
+ * @value: Value taken from file
+ * @i: Line in file
+ */
 
 void function_call(op_func function, char *code, char *value, int i)
 {
-    stack_t *node;
-    int j, check = 1;
+	stack_t *node;
+	int j, check = 1;
 
-    if (strcmp(code, "push") == 0)
-    {
+	if (strcmp(code, "push") == 0)
+	{
 /* check if value is negative and its diferent from NULL*/
-        if (value != NULL && value[0] == '-')
-        {
-            value += 1; 
-            check = -1;
-        }
-        if (value == NULL)
-        {
-            printf("L%d: usage: push integer\n", i);
-            exit(EXIT_FAILURE);
-        }
-        for(j = 0; value[j] != '\0'; j++)
-        {
-            if (isdigit(value[j]) == 0)
-            {
-                printf("L%d: usage: push integer\n", i);
-                exit(EXIT_FAILURE);
-            }
-        }
-        node = new_node(atoi(value) * check);
-        function(&node, i);
-    }
-    else
-    {
-        function(&head, i);
-    }
-
-
+		if (value != NULL && value[0] == '-')
+		{
+			value += 1;
+			check = -1;
+		}
+		if (value == NULL)
+		{
+		printf("L%d: usage: push integer\n", i);
+			exit(EXIT_FAILURE);
+		}
+		for (j = 0; valuei[j] != '\0'; j++)
+		{
+			if (isdigit(value[j]) == 0)
+			{
+				printf("L%d: usage: push integer\n", i);
+				exit(EXIT_FAILURE);
+			}
+		}
+		node = new_node(atoi(value) * check);
+		function(&node, i);
+	}
+	else
+	{
+		function(&head, i);
+	}
 }

c2a5765b6f8f61792d554c288dc20ca7a714f0a2
rmbodegalibre <rmbodegalibre@gmail.com>
2022-05-18 20:26:40 -0500

    igualado

--- tokenizar.c
+++ tokenizar.c
@@ -4,24 +4,29 @@
  * 
  * @line: Pointer to the current line
  * @i: Number of the line
+ * @check_token: Integer
  * @return int 
  */
 
-int tokenizar(char *line, int i)
+int tokenizar(char *line, int i, int check_token)
 {
     char *code;
     char *value;
 
     if (line == NULL)
-        printf("Error");
+    {
+        printf("Error: \n");
+        exit(EXIT_FAILURE);
+    }
+        
     code = strtok(line, "\t\n ");
     if (code == NULL)
         return (0);
     value = strtok(NULL, "\t\n ");
-/*Test if i got the code and the value right*/
-    printf("\n%s :%d", code, i);
-    printf("\n%s :%d\n", value, i);
-    /*get_function();*/
+/Test if i got the code and the value right/
+/*   printf("\n%s :%d", code, i);*/
+/*    printf("\n%s :%d\n", value, i);*/
+    get_function(code, value, i);
 
-return(0);
+return(check_token);
 }

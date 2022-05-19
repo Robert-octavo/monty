281cdfd7ce92296e5743340616f7d2b3d0bf8632
Robert-octavo <robert.ortega.octavo@gmail.com>
2022-05-18 19:36:31 -0500

    Function that creates a node

---
+++ new_node.c
@@ -0,0 +1,24 @@
+#include "monty.h"
+stack_t *head = NULL;
+/**
+ * new_node - function that creates a node
+ * @n: value of the node
+ * Return: Pointer to the new node or Null if it fails
+ */
+stack_t *new_node(int n)
+{
+    stack_t *new_node;
+
+    new_node = malloc(sizeof(stack_t));
+
+    if (new_node == NULL)
+    {
+        printf("Error: malloc failed\n");
+        exit(EXIT_FAILURE);
+    }
+
+    new_node->next = NULL;
+    new_node->prev = NULL;
+    new_node->n = n;
+    return (new_node);
+}

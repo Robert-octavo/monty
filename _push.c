880bafb9e3fc69045d95622c3f3b8ad929a62754
rmbodegalibre <rmbodegalibre@gmail.com>
2022-05-18 21:26:52 -0500

    _push.c passed by betty

--- _push.c
+++ _push.c
@@ -1,7 +1,7 @@
 #include "monty.h"
 /**
  * _push - function that add a node to the stack
- * @new: Pointer to the new node 
+ * @new: Pointer to the new node
  * @i: Line number
  */
 
@@ -9,18 +9,18 @@ void _push(stack_t **new, __attribute__((__unused__)) unsigned int i)
 {
 /*check if it enter to the function*/
 /*   printf("enter _push: %d", i);*/
-    stack_t *tmp;
-    /*(void) i;*/
-    if (*new == NULL)
-        exit(EXIT_FAILURE);
+	stack_t *tmp;
+	/*(void) i;*/
+	if (*new == NULL)
+		exit(EXIT_FAILURE);
 
-    if (head == NULL)
-    {
-        head = *new;
-        return;
-    }
-    tmp = head;
-    head = *new;
-    head->next = tmp;
-    tmp->prev = head;
+	if (head == NULL)
+	{
+		head = *new;
+		return;
+	}
+	tmp = head;
+	head = *new;
+	head->next = tmp;
+	tmp->prev = head;
 }

22ebfd47691fd309b857c35b4f300246d96f940d
Robert-octavo <robert.ortega.octavo@gmail.com>
2022-05-19 01:05:27 -0500

    Function that does nothing

---
+++ _nop.c
@@ -0,0 +1,11 @@
+#include "monty.h"
+/**
+ * _nop - The opcode nop doesn’t do anything.
+ * @stack: Is the first attribute
+ * @i: Line number
+ */
+void _nop(stack_t  **stack, unsigned int i)
+{
+	(void)stack;
+	(void)i;
+}

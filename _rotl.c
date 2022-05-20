cb4add6f39311affc6e17eb29b390963a766b755
Robert-octavo <robert.ortega.octavo@gmail.com>
2022-05-20 16:05:29 -0500

    betty style

--- _rotl.c
+++ _rotl.c
@@ -8,20 +8,20 @@
  */
 void _rotl(stack_t **stack, unsigned int i)
 {
-    stack_t *tmp;
+	stack_t *tmp;
 
+	(void)i;
 
-    (void)i;
-    if (stack == NULL || *stack == NULL || (*stack)->next == NULL)
-        return;
+	if (stack == NULL || *stack == NULL || (*stack)->next == NULL)
+		return;
 /*go to the last node*/
-    tmp = *stack;
-    while (tmp->next != NULL)
-        tmp = tmp->next;
+	tmp = *stack;
+	while (tmp->next != NULL)
+		tmp = tmp->next;
 
-    tmp->next = *stack;
-    (*stack)->prev = tmp;
-    *stack = (*stack)->next;
-    ((*stack)->prev)->next = NULL;
-    (*stack)->prev = NULL;
+	tmp->next = *stack;
+	(*stack)->prev = tmp;
+	*stack = (*stack)->next;
+	((*stack)->prev)->next = NULL;
+	(*stack)->prev = NULL;
 }

3b82a19553ea6d755aba798e501d34cfbd1c7b17
Robert-octavo <robert.ortega.octavo@gmail.com>
2022-05-19 22:30:02 -0500

    correct betty

--- _pstr.c
+++ _pstr.c
@@ -9,21 +9,21 @@ void _pstr(stack_t  **stack, unsigned int i)
 {
     /*to save the ASCII Value of the (n) in the stack*/
 	int value;
+	(void)i;
 
-    (void)i;
-    if (stack == NULL || *stack == NULL)
-    {
-        printf("\n"); 
-        return;
-    }
-    while (stack)
-    {
-        value = (*stack)->n;
-    /*value out of the ASCII range - stop */
-        if (value < 0 || value > 127)
-            break;
-        printf("%c", value);
-        *stack = (*stack)->next;
-    }
-    printf("/n");
+	if (stack == NULL || *stack == NULL)
+	{
+		printf("\n");
+		return;
+	}
+	while (stack)
+	{
+		value = (*stack)->n;
+/*value out of the ASCII range - stop */
+		if (value < 0 || value > 127)
+			break;
+		printf("%c", value);
+		*stack = (*stack)->next;
+	}
+	printf("/n");
 }

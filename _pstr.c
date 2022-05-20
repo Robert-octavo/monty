fb0d86f2e6f807be8243d835817477e295ffb985
Robert-octavo <robert.ortega.octavo@gmail.com>
2022-05-19 22:44:10 -0500

    change confition, add a *tmp

--- _pstr.c
+++ _pstr.c
@@ -9,6 +9,7 @@ void _pstr(stack_t  **stack, unsigned int i)
 {
     /*to save the ASCII Value of the (n) in the stack*/
 	int value;
+    stack_t *tmp;
 	(void)i;
 
 	if (stack == NULL || *stack == NULL)
@@ -16,14 +17,16 @@ void _pstr(stack_t  **stack, unsigned int i)
 		printf("\n");
 		return;
 	}
-	while (stack)
+    tmp = *stack;
+	while (tmp)
 	{
-		value = (*stack)->n;
+		value = tmp->n;
 /*value out of the ASCII range - stop */
-		if (value < 0 || value > 127)
+/*if value gets 0, it ends*/
+		if (value < 1 || value > 127)
 			break;
 		printf("%c", value);
-		*stack = (*stack)->next;
+		tmp = tmp->next;
 	}
-	printf("/n");
+	printf("\n");
 }
